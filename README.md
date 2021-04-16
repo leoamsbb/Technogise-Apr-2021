8b7f4d90009c3a8e11d1cd404046d71c21a0b175
Mrunal <45357473+leoamsbb@users.noreply.github.com>
2021-04-16 12:24:13 +0530

    Piece and position for ChessPiece (#1)
    * Store Position of chess piece in row and column format
    
    * Added tools with versions and commands to run tests in ReadMe.md
    
    * We should be able to create object of chess pieces using the input string
    
    * Throw invalid input exception with simple message about usage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Technogise-Apr-2021
+- [Tools](#tools)
+- [Running Tests](#running-tests)
+
+## Tools
+| Tech / Tools / Libraries | version |
+| ---------- | ------- |
+| sbt | 1.4.3 |
+| Java | 1.8_0_202 |
+| Scala | 2.13.5 |
+| ScalaTest | 3.0.8 |
+
+## Running Tests
+Command to execute in Terminal:
+```
+sbt test
+```
---
+++ src/main/scala/com/github/leoamsbb/chess/Position.scala
@@ -0,0 +1,14 @@
+package com.github.leoamsbb.chess
+
+case class Position(row: Char, column: Int) {
+  override def toString: String = s"$row$column"
+}
+
+object Position {
+  implicit def fromString(strPosition: String) : Position =
+    Position(
+      strPosition.charAt(0),
+      Integer.parseInt(strPosition.charAt(1).toString)
+    )
+}
+
