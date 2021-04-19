b4a1e452051c821dd85afa1eb51f7cfc73b864c2
leoamsbb <mrunalbadhe24@gmail.com>
2021-04-19 18:51:12 +0530

    Added Position Mathematics

--- src/main/scala/com/github/leoamsbb/chess/Position.scala
+++ src/main/scala/com/github/leoamsbb/chess/Position.scala
@@ -4,6 +4,12 @@ import scala.language.implicitConversions
 
 case class Position(row: Char, column: Int) {
   override def toString: String = s"$row$column"
+
+  def increaseRow : Position = copy(row = (row.toInt + 1).toChar)
+  def decreaseRow : Position = copy(row = (row.toInt - 1).toChar)
+
+  def increaseColumn : Position = copy(column = column + 1)
+  def decreaseColumn : Position = copy(column = column - 1)
 }
 
 object Position {
--- src/test/scala/com/github/leoamsbb/chess/PositionSpec.scala
+++ src/test/scala/com/github/leoamsbb/chess/PositionSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FunSpec, Matchers}
 
 class PositionSpec extends FunSpec with Matchers {
 
-  describe("Position") {
+  describe("Position Extraction") {
     it("should be able to separate String into row and column type") {
       val pos: Position = "D3"
       pos.row shouldBe 'D'
@@ -14,6 +14,28 @@ class PositionSpec extends FunSpec with Matchers {
     it("display position in proper output format") {
       val pos: Position = Position('C', 4)
       pos.toString shouldBe "C4"
+    }
+  }
+
+  describe("Position Mathematics") {
+    it("should return position from next row and same column") {
+      val position:Position = "D3"
+      position.increaseRow.toString shouldBe "E3"
+    }
+
+    it("should return position from previous row and same column") {
+      val position:Position = "D3"
+      position.decreaseRow.toString shouldBe "C3"
+    }
+
+    it("should return position from same row but next column") {
+      val position:Position = "D3"
+      position.increaseColumn.toString shouldBe "D4"
+    }
+
+    it("should return position from same row but previous column") {
+      val position:Position = "D3"
+      position.decreaseColumn.toString shouldBe "D2"
     }
   }
 }
