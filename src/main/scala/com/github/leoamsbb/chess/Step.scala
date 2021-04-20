b9405427c8ee63c53ab54defbe88b58b592738fd
leoamsbb <mrunalbadhe24@gmail.com>
2021-04-20 16:48:49 +0530

    Implemented Two and Half Step for all directions

--- src/main/scala/com/github/leoamsbb/chess/Step.scala
+++ src/main/scala/com/github/leoamsbb/chess/Step.scala
@@ -30,4 +30,27 @@ case object Single extends Step {
   }
 }
 
-case object TwoAndHalf extends Step
+case object TwoAndHalf extends Step {
+  private val verticalHalfSteps = List((_: Position).increaseColumn, (_: Position).decreaseColumn)
+  private val horizontalHalfSteps = List((_: Position).increaseRow, (_: Position).decreaseRow)
+
+  override def advance(current: Position, direction: Direction): List[Position] = direction match {
+    case Vertical(true, _) =>
+      val twoSteps = (_: Position).increaseRow.increaseRow
+      verticalHalfSteps.map(twoSteps andThen _).map(_ (current))
+    case Horizontal(true, _) =>
+      val twoSteps = (_: Position).increaseColumn.increaseColumn
+      horizontalHalfSteps.map(twoSteps andThen _).map(_ (current))
+    case _ => Nil
+  }
+
+  override def retreat(current: Position, direction: Direction): List[Position] = direction match {
+    case Vertical(_, true) =>
+      val twoSteps = (_: Position).decreaseRow.decreaseRow
+      verticalHalfSteps.map(twoSteps andThen _).map(_ (current))
+    case Horizontal(_, true) =>
+      val twoSteps = (_: Position).decreaseColumn.decreaseColumn
+      horizontalHalfSteps.map(twoSteps andThen _).map(_ (current))
+    case _ => Nil
+  }
+}
--- src/test/scala/com/github/leoamsbb/chess/StepSpec.scala
+++ src/test/scala/com/github/leoamsbb/chess/StepSpec.scala
@@ -10,30 +10,49 @@ class StepSpec extends FunSpec with Matchers {
 
   describe("Single Step") {
     it("should return correct position when taken in vertical forward direction") {
-      val actual = Single.advance("D5", vertical).map(_.toString)
-      actual should contain theSameElementsAs List("E5")
+      verifyOutput(Single.advance("D5", vertical), List("E5"))
     }
     it("should return correct position when taken in vertical backward direction") {
-      val actual = Single.retreat("D5", vertical).map(_.toString)
-      actual should contain theSameElementsAs List("C5")
+      verifyOutput(Single.retreat("D5", vertical), List("C5"))
     }
 
     it("should return correct position when taken in horizontal forward direction") {
-      val actual = Single.advance("D5", horizontal).map(_.toString)
-      actual should contain theSameElementsAs List("D6")
+      verifyOutput(Single.advance("D5", horizontal), List("D6"))
     }
     it("should return correct position when taken in horizontal backward direction") {
-      val actual = Single.retreat("D5", horizontal).map(_.toString)
-      actual should contain theSameElementsAs List("D4")
+      verifyOutput(Single.retreat("D5", horizontal), List("D4"))
     }
 
     it("should return correct position when taken in diagonal forward direction") {
-      val actual = Single.advance("D5", diagonal).map(_.toString)
-      actual should contain theSameElementsAs List("E4", "E6")
+      verifyOutput(Single.advance("D5", diagonal), List("E4", "E6"))
     }
     it("should return correct position when taken in diagonal backward direction") {
-      val actual = Single.retreat("D5", diagonal).map(_.toString)
-      actual should contain theSameElementsAs List("C4", "C6")
+      verifyOutput(Single.retreat("D5", diagonal), List("C4", "C6"))
     }
+  }
+
+  describe("Two and Half Step") {
+    it("should return correct position when taken in vertical forward direction") {
+      verifyOutput(TwoAndHalf.advance("E3", vertical), List("G2", "G4"))
+    }
+    it("should return correct position when taken in vertical backward direction") {
+      verifyOutput(TwoAndHalf.retreat("E3", vertical), List("C2", "C4"))
+    }
+
+    it("should return correct position when taken in horizontal forward direction") {
+      verifyOutput(TwoAndHalf.advance("E3", horizontal), List("F5", "D5"))
+    }
+    it("should return correct position when taken in horizontal backward direction") {
+      verifyOutput(TwoAndHalf.retreat("E3", horizontal), List("F1", "D1"))
+    }
+
+    it("should return empty list when taken in either diagonal direction") {
+      TwoAndHalf.advance("E3", diagonal) shouldBe empty
+      TwoAndHalf.retreat("E3", diagonal) shouldBe empty
+    }
+  }
+
+  private def verifyOutput(actual: List[Position], expected: List[String]): Unit = {
+    actual.map(_.toString) should contain theSameElementsAs expected
   }
 }
