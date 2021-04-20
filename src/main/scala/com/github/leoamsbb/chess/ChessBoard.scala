7895f3b6547885b9acdb399392f3dddc1fde513d
leoamsbb <mrunalbadhe24@gmail.com>
2021-04-20 17:16:55 +0530

    Added a utility to check whether a position is within or outside board

---
+++ src/main/scala/com/github/leoamsbb/chess/ChessBoard.scala
@@ -0,0 +1,12 @@
+package com.github.leoamsbb.chess
+
+object ChessBoard {
+  def withinBoard(input: Position) =
+    input.row <= 'H' &&
+      input.row >= 'A' &&
+      input.column <= 8 &&
+      input.column >= 1
+
+  def outsideBoard(input: Position): Boolean = !withinBoard(input)
+
+}
---
+++ src/test/scala/com/github/leoamsbb/chess/ChessBoardSpec.scala
@@ -0,0 +1,27 @@
+package com.github.leoamsbb.chess
+
+import com.github.leoamsbb.chess.ChessBoard.{outsideBoard, withinBoard}
+import org.scalatest.{FunSpec, Matchers}
+
+class ChessBoardSpec extends FunSpec with Matchers {
+
+  describe("withinBoard") {
+    it("should return true when the given position is inside board") {
+      withinBoard("D3") shouldBe true
+    }
+
+    it("should return false when the given position is outside board") {
+      withinBoard("H0") shouldBe false
+    }
+  }
+
+  describe("outsideBoard") {
+    it("should return true when the given position is outside board") {
+      outsideBoard("A9") shouldBe true
+    }
+
+    it("should return false when the given position is inside board") {
+      outsideBoard("A2") shouldBe false
+    }
+  }
+}
