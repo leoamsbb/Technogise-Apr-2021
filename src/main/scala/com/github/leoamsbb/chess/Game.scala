4c68df26d320df40bce10922510923b09ce94baf
leoamsbb <mrunalbadhe24@gmail.com>
2021-04-21 12:32:31 +0530

    So that the output is printed as String instead of List

--- src/main/scala/com/github/leoamsbb/chess/Game.scala
+++ src/main/scala/com/github/leoamsbb/chess/Game.scala
@@ -3,5 +3,5 @@ package com.github.leoamsbb.chess
 import scala.io.StdIn.readLine
 
 object Game extends App {
-  println(ChessPiece.fromString(readLine()).possibleMoves)
+  println(ChessPiece.fromString(readLine()).possibleMoves.mkString(", "))
 }
